22517c2ee310192b0d1d5741d054e97a86e62ec9
Kronos0477 <kronos.0477@gmail.com>
2025-01-23 16:06:12 +0530

    Update db.js

--- Backened/db.js
+++ Backened/db.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-const mongouri = 'mongodb+srv://kronos0477:manyug4908@kronos-first.j23r4.mongodb.net/Inotebook?retryWrites=true&w=majority&appName=Kronos-First';
-
+const mongouri = 'Enter Your Mongodb Uri'
 const connecttomongo = async () => {
     try {
         await mongoose.connect(mongouri);
